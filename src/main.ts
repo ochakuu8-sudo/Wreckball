4ea26635a5628985f1bcf6d0ae55830a8f59d639
ochakuu8-sudo <ochakuu8@gmail.com>
2026-05-01 13:28:33 +0900

    Use animated flipper rampage mode

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { RampageGame } from './rampage-game-inlanes';
+import { RampageGame } from './rampage-game-animated';
 import { initSdk } from './sdk';
 
 const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
